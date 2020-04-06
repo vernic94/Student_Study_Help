62d4f7518b3ee43ade25ff734c4028ea5666ddaa
sagah <sagah@kth.se>
2020-04-06 14:29:03 +0200

    login & create user added

--- src/CreateAccount/createaccount.js
+++ src/CreateAccount/createaccount.js
@@ -1,4 +1,4 @@
-/* Create account page (accessible from Welcome page - Sign up button) 
+/* Create account page (accessible from Welcome page - Sign up button)
 
 Responsible: Saga
 
@@ -10,7 +10,7 @@ Should consist of:
 - header ?
 - Side/top bar (?)
 =======
-Should consist of: 
+Should consist of:
 - You should be able to type in parameters (input function), email, password
 - Create account/Sign up button, when onClick -> profile page
 - header
@@ -22,46 +22,70 @@ Must not be handled (this iteration):
 
 */
 
-<<<<<<< HEAD
 import React, { Component } from 'react';
-/*import { Link } from "react-router-dom";*/
+import { Link } from "react-router-dom";
+import Topbar from "../Topbar/topbar"
+
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
 
 class CreateAccount extends Component{
+	constructor(props){
+	  super(props);
+	  this.state={
+		users: [],
+	    username:"",
+	    password: "",
+	    correct: false,
+	    change: false,
+	    status: "NULL"
+	  }
+    }
+    addHandler=()=>{
+		this.createUser();
+    }
+
+    userExists(){
+		/*check if user already exists*/
+	}
+
+    confirmPassword(){
+		/*check confirm password*/
+	}
+
+    createUser(){
+		//let users = this.state.users;
+	    bcrypt.hash(this.state.password, saltRounds, (err, hash) => {
+			this.setState({users: [this.state.username,hash]});
+		});
+		console.log(this.state.users);
+	}
+
 	render() {
-	return (
- 	  <div className="CreateAccount">
-		<label>Email:</label>
-		<input type="text" />
-		<label>Password:</label>
-		<input type="password" />
-		<label>Confirm password:</label>
-		<input type="password" />
+		let msg = null;
+		if ( this.state.change==true&&this.state.password.length<8 ){
+			msg = <p>your password should be at least 8 characters long</p>
+		}
+		return (
 
+ 		  <div className="CreateAccount">
+ 		  	<Topbar/>
+ 		  	<h1>Create Account</h1>
+			<label>Email:</label>
+			<input type="text" onChange={e=> this.setState({username: e.target.value})}/>
+			<label>Password:</label>
+			<input type="password" onChange={e=> this.setState({password: e.target.value, change: true})}/>
+			{msg}
+			<label>Confirm password:</label>
+			<input type="password" />
 
+			<Link to="/profile" onClick={this.addHandler}>
+				<button className="logBtn">Create user</button>
+   		    </Link>
 
-		<a href="https://kth.instructure.com/">
-		            <button className="logBtn">Create user</button>
-        </a>
-
-	  </div>
-	 );
+	  	  </div>
+		);
  }
 }
 
 export default CreateAccount;
-
-=======
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./createaccount.css";
-
-class CreateAccount extends Component {
-  render() {
-    return (
-      <h1>CREATE ACCOUNT PAGE</h1>
-    );
-  }
-}
-
-export default CreateAccount;
->>>>>>> master
--- src/Welcome/welcome.js
+++ src/Welcome/welcome.js
@@ -1,11 +1,11 @@
-/* Welcome page/Start page 
+/* Welcome page/Start page
 
 ITERATION 2
-Should consist of: 
+Should consist of:
 - Create account/Sign up button that leads to "Create account" page
 - You should be able to type in username and password (input function)
 - Login button, when onClick -> map page
-- header 
+- header
 
 Must not be handled (this iteration):
 - Account username and password from database, and therefore not 100% functional login function
@@ -16,10 +16,37 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./welcome.css";
 
+
+
 class Welcome extends Component {
+	constructor(props){
+	  super(props);
+	  this.state={
+	    username:"",
+	    password: ""
+	  }
+    }
+  checkPassword(){
+	/*make sure password match user*/
+  }
   render() {
     return (
-      <h1>WELCOME PAGE</h1>
+ 	  <div className="Welcome">
+ 	  	<h1>WELCOME PAGE</h1>
+		<label>Email:</label>
+		<input type="text" />
+		<label>Password:</label>
+		<input type="password" />
+
+		<Link to="/profile">
+			<button className="logBtn">Log in</button>
+        </Link>
+        <Link to="/create-account">
+			<button className="logBtn">Create user</button>
+        </Link>
+
+	  </div>
+
     );
   }
 }
