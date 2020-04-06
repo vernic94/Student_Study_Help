6d546a3f3344a799f53defb275abd76cab5f8464
sagah <sagah@kth.se>
2020-04-06 09:15:20 +0200

    create account basic structure

--- src/CreateAccount/createaccount.js
+++ src/CreateAccount/createaccount.js
@@ -1,8 +1,8 @@
 /* Create account page (accessible from Welcome page - Sign up button)
 
 ITERATION 2
-Should consist of: 
-- You should be able to type in parameters (input function), username?, email?, password, school (list?) 
+Should consist of:
+- You should be able to type in parameters (input function), username?, email?, password, school (list?)
 - Login button, when onClick -> profile page ?
 - header ?
 - Side/top bar (?)
@@ -11,3 +11,31 @@ Must not be handled (this iteration):
 - Store user in database
 
 */
+
+import React, { Component } from 'react';
+/*import { Link } from "react-router-dom";*/
+
+class CreateAccount extends Component{
+	render() {
+	return (
+ 	  <div className="CreateAccount">
+		<label>Email:</label>
+		<input type="text" />
+		<label>Password:</label>
+		<input type="password" />
+		<label>Confirm password:</label>
+		<input type="password" />
+
+
+
+		<a href="https://kth.instructure.com/">
+		            <button className="logBtn">Create user</button>
+        </a>
+
+	  </div>
+	 );
+ }
+}
+
+export default CreateAccount;
+
