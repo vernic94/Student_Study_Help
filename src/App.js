79f6f15e4cad4d78e3644a7ca128aeffac985aa8
loujonsson <43542444+loujonsson@users.noreply.github.com>
2020-04-06 11:03:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,19 +15,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
 
         <Route exact path="/" component={Welcome} />
         <Route path="/aboutus" component={AboutUs}/>
