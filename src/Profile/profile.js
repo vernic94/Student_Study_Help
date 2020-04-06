86bdf2b10c6160eb79755d57430a97359e2eb4ae
agnesaltin <43341692+agnesaltin@users.noreply.github.com>
2020-04-06 10:13:48 +0200

    profile editor

--- src/Profile/profile.js
+++ src/Profile/profile.js
@@ -14,3 +14,31 @@ Must not be handled (this iteration):
 - Get user information from database 
 
 */
+
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./profile.css";
+
+class Profile extends Component {
+	//constructor(props){}
+	
+	render(){
+		return(
+            <div className="ProfileContainer">
+                <div className="Edit">
+                    <button type="button">Edit Profile</button>
+                </div>
+                <h1>Username</h1>
+                <div>
+                    <p><i>Biography...</i></p>
+                    <p>School</p>
+                    <p>Subjects</p>
+                    <p>Classes</p>
+                    <p>Study sessions</p>
+                </div>
+            </div>
+        );
+	}
+}
+
+export default Profile;
---
+++ src/ProfileEditor/profileEditor.js
@@ -0,0 +1,65 @@
+/* Profile Editor
+
+Responsible: Agnes
+
+ITERATION 2
+Should consist of: 
+- username efitor
+- biography editor
+- school editor
+- subjects
+- courses
+- save button
+- cancel button 
+- return to profile
+- header 
+- Side/top bar 
+
+*/
+
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./profileEditor.css";
+
+class ProfileEditor extends Component {
+	//constructor(props){}
+	
+	render(){
+		return(
+            <div className="ProfileEditorContainer">
+                <button type="button">Return to Profile</button>
+                <div className="UsernameEditor">
+                    <label for="fname">Username:</label>
+                    <input type="text" id="username" name="username"></input>
+                </div>
+                <div className="BiographyEditor">
+                    <label for="fname">Biography:</label>
+                    <input type="text" id="biography" name="biography"></input>
+                </div>
+                <div className="SchoolEditor">
+                    <select id="schools">
+                        <label for="subjects">Select a school:</label>
+                        <option value="kth">Kungliga Tekniska Högskolan</option>
+                        <option value="su">Stockholms Universitet</option>
+                        <option value="ki">Karonlinska</option>
+                    </select>
+                </div>
+                <div className="SubjectEditor">
+                    <label for="subjects">Select a subject:</label>
+                    <select id="subjects">
+                        <option value="maths">Maths</option>
+                        <option value="biology">Biology</option>
+                        <option value="economy">Economy</option>
+                        <option value="history">History</option>
+                    </select>
+                </div>
+                <div className="CoursesEditor">
+                </div>
+                <button type="button">Save Changes</button>
+                <button type="button">Cancel</button>
+            </div>
+        );
+	}
+}
+
+export default ProfileEditor;
