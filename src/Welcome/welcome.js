49002a1efa4b0e508df4ba37c573833d68bfdcf3
Fariba Yazdani <fariba.yazdani11@gmail.com>
2020-04-07 20:35:19 +0200

    adding login page

--- src/Welcome/welcome.js
+++ src/Welcome/welcome.js
@@ -12,16 +12,40 @@ Must not be handled (this iteration):
 
 */
 
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
 import "./welcome.css";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 class Welcome extends Component {
-  render() {
-    return (
-      <h1>WELCOME PAGE</h1>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>WELCOME PAGE</h1>
+                <form className="form-signin">
+                    <img className="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72"
+                         height="72">
+                    </img>
+                    <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                    <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required
+                           autoFocus>
+                    </input>
+                    <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password"
+                           required></input>
+
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                </form>
+                <div className="mb-3">
+                    <label><h5>To create an account click here.</h5>
+                    </label>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Welcome;
