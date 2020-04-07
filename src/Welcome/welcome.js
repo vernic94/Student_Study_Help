5aa32bd47cccb3fb582f34fcca5e0a5b4b91bc5c
Fariba Yazdani <fariba.yazdani11@gmail.com>
2020-04-07 20:53:59 +0200

    adding logo

--- src/Welcome/welcome.js
+++ src/Welcome/welcome.js
@@ -23,20 +23,17 @@ class Welcome extends Component {
     render() {
         return (
             <div>
-                <h1>WELCOME PAGE</h1>
+                <img className="mb-4" src={require("../images/student-study-help-1.png")}
+                     height="200vh">
+                </img>
                 <form className="form-signin">
-                    <img className="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72"
-                         height="72">
-                    </img>
                     <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
                     <label htmlFor="inputEmail" className="sr-only">Email address</label>
                     <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required
                            autoFocus>
                     </input>
                     <label htmlFor="inputPassword" className="sr-only">Password</label>
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password"
-                           required></input>
-
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
                     <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
                 </form>
                 <div className="mb-3">
