5b7d75ed99a6dfd64c04a2c47e591ef272c8b528
loujons <loujons@kth.se>
2020-04-08 22:45:39 +0200

    Revert "Amanda"

--- src/StudySession/studysession.js
+++ src/StudySession/studysession.js
@@ -19,67 +19,9 @@ import { Link } from "react-router-dom";
 import "./studysession.css";
 
 class StudySession extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: "LOADING"
-    };
-  }
-
-  createStudySession(){
-    this.setState({
-      status: "CREATE"
-    }); 
-  }
-
-  planStudySession(){
-    console.log("hrjgkdfbb");
-    
-    this.setState({
-      status: "PLAN"
-    });
-  }
-
   render() {
-    let studySessionParameters = null;
-
-    switch(this.state.status) {
-      case "LOADING":
-        studySessionParameters = null;
-        break;
-      case "CREATE":
-        studySessionParameters = 
-        <div className="location-parameter">
-          <p>Location:</p>
-          <div className="btnDiv">
-            <button onClick={() => this.function()}>Create a study session</button>
-          </div>
-        </div>;
-        break;
-      case "PLAN":
-        studySessionParameters = 
-        <form action="action.php">
-        mkojuW id : <input type="text" name="email" /><br />
-        Password : <input type="text" name="passw" /><br />
-        <input type="submit" value="Submit" />
-        </form>;
-        break;
-      default:
-        studySessionParameters = <p>Failed to load, please try again</p>  
-        break; 
-    }
-
-
     return (
-      <div className="studysession">
-        <h1>CREATE STUDY SESSION PAGE</h1>
-        <p>Create a current study session or plan a study session in the future!</p>
-        <button onClick={() => this.createStudySession()}>Create a study session</button>
-        <button onClick={() => this.planStudySession()}>Plan a study session</button>
-        <div className="studySessionParameters">
-          {studySessionParameters}
-        </div>
-      </div>
+      <h1>CREATE STUDY SESSION PAGE</h1>
     );
   }
 }
--- src/Welcome/welcome.js
+++ src/Welcome/welcome.js
@@ -1,49 +1,27 @@
-/* Welcome page/Start page
+/* Welcome page/Start page 
 
 ITERATION 2
-Should consist of:
+Should consist of: 
 - Create account/Sign up button that leads to "Create account" page
 - You should be able to type in username and password (input function)
 - Login button, when onClick -> map page
-- header
+- header 
 
 Must not be handled (this iteration):
 - Account username and password from database, and therefore not 100% functional login function
 
 */
 
-import React, {Component} from "react";
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./welcome.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 class Welcome extends Component {
-    render() {
-        return (
-            <div>
-                <img className="mb-4" src={require("../images/student-study-help-1.png")}
-                     height="200vh">
-                </img>
-                <form className="form-signin">
-                    <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                    <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required
-                           autoFocus>
-                    </input>
-                    <label htmlFor="inputPassword" className="sr-only">Password</label>
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
-                    <button className="btn btn-lg  btn-block" type="submit"
-                            style={{backgroundColor: "#56CCF2"}}>Sign in</button>
-                </form>
-                <div className="mb-3">
-                    <label><h5>To create an account click here.</h5>
-                    </label>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <h1>WELCOME PAGE</h1>
+    );
+  }
 }
 
 export default Welcome;
