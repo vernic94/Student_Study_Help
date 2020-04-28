47ab502bfd19cec18a7a21332d28664aef755681
abaz <abaz@kth.se>
2020-04-28 14:16:47 +0200

    CSS

--- src/AboutUs/aboutus.js
+++ src/AboutUs/aboutus.js
@@ -1,11 +1,4 @@
 /* About us page
-
-ITERATION 2
-Should consist of:
-- names, kth email?
-- picture of ourselves (if you are comfortable with it)
-- Maybe short description of why we are making this project
-
 */
 
 import React, { Component } from "react";
@@ -15,57 +8,58 @@ import Topbar from "../Topbar/topbar"
 
 class AboutUs extends Component {
 
-  render() {
-    return (
+    render() {
+        return (
 
-      <div className="AboutUs">
-        <Topbar/>
-          <img class="AboutUsIcon" src={require("../images/student-study-help-2.png")}
-               height="150vh">
-          </img>
-        <h1 class="AboutUsH1">ABOUT US</h1>
-          <div class="Boxed">
-        <p>
+            <div className="AboutUs">
+                <Topbar/>
+                <div className="Boxed">
+                    <br/>
+                    <img class="AboutUsIcon" src={require("../images/student-study-help-3.png")}
+                         height="150vh">
+                    </img>
+                    <h1 class="AboutUsH1">ABOUT US</h1>
+                    <p>
 
-            <i>Study Group</i>, an application to find study groups.
-            <br/>
-            Created by six students from KTH Royal Institute of Technology.
-            <br/><br/><h3 class="AboutUsH3" >By students, for students</h3>
-            <p className="BoxedText">
-            As students we know how necessary it can be to study with others.
-            <br/>&emsp;Discussing questions, solutions and perspectives on a subject
-            with others can make the subject feel easier to understand
-                and study. We wanted to make it easier for students to find a <i>study group</i>,
-                and therefore created this application. </p>
-            <br/><h3 class="AboutUsH3" >What is <i>Study Group</i>?</h3>
-            <p className="BoxedText">
-            The short answer, well an application to find study groups.
-                <br/>&emsp;Finding people to study with isn't always the easiest.
-            Some of us recently moved from another city, or even country, and
-            don't know anyone in school.
-            Some maybe prefer to study in another campus,
-            or maybe have a hard time socializing.
-            Regardless of the reason, one can use this app to find where and when people
-            are studying, get started by clicking <i>Find Study Sessions</i> at the top!
-            </p>
-        </p>
-        <br/>
-        <h3 class="AboutUsH3">Contact information</h3>
-              <p class="UnderH3">- We who worked on <i>Study Group</i></p>
-              <div  class="ContactInfo">
-              <br/>Agnes Altin<br/>Email: Agnesal@kth.se<br/>
-              <br/>Amanda Baza<br/>Email: Abaz@kth.se<br/>
-              <br/>Fariba Yazdani Rasouli<br/>Email: Faribayr@kth.se<br/>
-              <br/>Lou Jönsson<br/>Email: Loujons@kth.se<br/>
-              <br/>Saga Harnesk<br/>Email: Sagah@kth.se<br/>
-              <br/>Veronika Nicolaou<br/>Email: Vernic@kth.se<br/>
+                        <i>Study Group</i>, an application to find study groups.
+                        <br/>
+                        Created by six students from KTH Royal Institute of Technology.
+                        <br/><br/><h3 class="AboutUsH3" >By students, for students</h3>
+                        <p className="BoxedText">
+                            As students we know how necessary it can be to study with others.
+                            <br/>&emsp;Discussing questions, solutions and perspectives on a subject
+                            with others can make the subject feel easier to understand
+                            and study. We wanted to make it easier for students to find a <i>study group</i>,
+                            and therefore created this application. </p>
+                        <br/><h3 class="AboutUsH3" >What is <i>Study Group</i>?</h3>
+                        <p className="BoxedText">
+                            The short answer, well an application to find study groups.
+                            <br/>&emsp;Finding people to study with isn't always the easiest.
+                            Some of us recently moved from another city, or even country, and
+                            don't know anyone in school.
+                            Some maybe prefer to study in another campus,
+                            or maybe have a hard time socializing.
+                            Regardless of the reason, one can use this app to find where and when people
+                            are studying, get started by clicking <i>Find Study Sessions</i> at the top!
+                        </p>
+                    </p>
+                    <br/>
+                    <h3 class="AboutUsH3">Contact information</h3>
+                    <p class="UnderH3">- We who worked on <i>Study Group</i></p>
+                    <div  class="ContactInfo">
+                        <br/>Agnes Altin<br/>Email: Agnesal@kth.se<br/>
+                        <br/>Amanda Baza<br/>Email: Abaz@kth.se<br/>
+                        <br/>Fariba Yazdani Rasouli<br/>Email: Faribayr@kth.se<br/>
+                        <br/>Lou Jönsson<br/>Email: Loujons@kth.se<br/>
+                        <br/>Saga Harnesk<br/>Email: Sagah@kth.se<br/>
+                        <br/>Veronika Nicolaou<br/>Email: Vernic@kth.se<br/>
 
-              <br/><br/>
-              </div>
-          </div>
-      </div>
-    );
-  }
+                        <br/><br/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default AboutUs;
--- src/Topbar/topbar.js
+++ src/Topbar/topbar.js
@@ -1,49 +1,48 @@
 import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom'
 import {Navbar, Nav} from 'react-bootstrap'
 import { Link } from "react-router-dom";
 import "./topbar.css";
 import modelInstance from "../data/Model";
 
 /*
-    Code to handle the topbar logic, which is a
-    navigation/menu sticked on top where the
-    user can navigate between the pages: "Home",
-    "Profile", "Create Study session", "Map" and "About".
+   Code to handle the topbar logic, which is a
+   navigation/menu sticked on top where the
+   user can navigate between the pages: "Home",
+   "Profile", "Create Study session", "Map" and "About".
 
-    2020-04-06
-    @Aurthor Amanda Baza
+   2020-04-06
+   @Aurthor Amanda Baza
 */
 
 export class Topbar extends React.Component{
     logoutHandler=()=>{
-		modelInstance.logout();
+        modelInstance.logout();
     }
 
     render(){
         return(
             <Navbar bg="dark" className="topbar">
-                <Navbar.Toggle />
                 <Navbar.Collapse>
-                    <Nav class="topbar-nav">
-                                {/*
-                                Navigation links where "to" is what
-                                page the link navigates to
-                                and "&nbsp;" adds a space after the Link.
-                                */}
-                                <Link class="topbar-button" to="profile">Profile</Link>&nbsp;
-                                <Link class="topbar-button" to="create-study-session">Create Study Session</Link>&nbsp;
-                                <Link class="topbar-button" to="find-study-session">Find Study Session</Link>&nbsp;
-                                <Link class="topbar-button" to="aboutus">About Us</Link>&nbsp;
-                                <Link class="topbar-button" to="/" onClick={this.logoutHandler}>Logout</Link>
+                    <Nav className="topbar-nav">
+                        <img className="topbar-icon" src={require("../images/student-study-help-3.png")}
+                             height="35vh">
+                        </img>&nbsp;
+                        {/*
+                               Navigation links where "to" is what
+                               page the link navigates to
+                               and "&nbsp;" adds a space after the Link.
+                               */}
+                        <Link className="topbar-button" to="profile">Profile</Link>&nbsp;
+                        <Link className="topbar-button" to="create-study-session">Create Study Session</Link>&nbsp;
+                        <Link className="topbar-button" to="find-study-session">Find Study Session</Link>&nbsp;
+                        <Link className="topbar-button" to="aboutus">About Us</Link>&nbsp;
+                        <Link className="topbar-button" to="/" onClick={this.logoutHandler}>Logout</Link>&nbsp;
 
                     </Nav>
                 </Navbar.Collapse>
-                <img className="topbar-icon" src={require("../images/student-study-help-2.png")}
-                     height="35vh">
-                </img>
-            </Navbar>     
-            );
+            </Navbar>
+        );
     }
 }
-export default Topbar;
-
+export default Topbar
