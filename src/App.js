f37ce306c50488b7199e6f2122c02e2ef8a94992
loujons <loujons@kth.se>
2020-04-29 09:32:56 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Welcome from "./Welcome/welcome";
 import CreateAccount from "./CreateAccount/createaccount";
 import AboutUs from "./AboutUs/aboutus";
 import Profile from "./Profile/profile";
+import ProfileEditor from "./ProfileEditor/profileEditor";
 import Maps from "./Maps/maps";
 import StudySession from "./StudySession/studysession";
-import ProfileEditor from "./ProfileEditor/profileEditor"
-import StudySessions from "./studysessions/studysessions";
+import FindStudySession from "./FindStudySession/findstudysession";
 import {Topbar} from './Topbar/topbar'
 import { Link } from "react-router-dom";
 
@@ -20,12 +20,11 @@ function App() {
 
         <Route exact path="/" component={Welcome} />
         <Route path="/aboutus" component={AboutUs}/>
-        <Route path="/find-study-session" component={Maps}/>
+        <Route path="/find-study-session" component={FindStudySession}/>
         <Route path="/profile" component={Profile}/>
         <Route path="/profileEditor" component={ProfileEditor}/>
         <Route path="/create-study-session" component={StudySession}/>
         <Route path="/create-account" component={CreateAccount}/>
-        <Route path="/study-sessions" component={StudySessions}/>
 
       </header>
     </div>
