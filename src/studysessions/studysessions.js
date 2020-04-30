e3517fd80e504393a6696a438a16ea4147eb2151
loujons <loujons@kth.se>
2020-04-30 10:48:04 +0200

    Update studysessions.js

--- src/studysessions/studysessions.js
+++ src/studysessions/studysessions.js
@@ -3,6 +3,16 @@ import {Link} from "react-router-dom";
 import "./studysessions.css";
 import {firebaseConfig} from "../data/firebaseConfig";
 
+export const firebaseConfig = {
+    apiKey: "AIzaSyDYL1p7zMpUYF4q0i7HLh6fvhFsQzOEoBM",
+    authDomain: "student-study-help.firebaseapp.com",
+    databaseURL: "https://student-study-help.firebaseio.com/",
+    projectId: "student-study-help",
+    storageBucket: "student-study-help.appspot.com",
+    messagingSenderId: "284363914579",
+    appId: "1:284363914579:web:7bec55fc128b5ab3cb35a6",
+    measurementId: "G-YPH7CP209E"
+};
 
 class StudySessions extends React.Component {
 
