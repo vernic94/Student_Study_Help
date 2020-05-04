d3622baeaaf650becfaeac39afd7ac911aa7baa1
abaz <abaz@kth.se>
2020-05-04 14:01:49 +0200

    topbar-tests (not done)

--- src/Topbar/topbar.js
+++ src/Topbar/topbar.js
@@ -1,7 +1,6 @@
 
 import React, {Component} from 'react';
 import {Navbar, Nav} from 'react-bootstrap'
-import { Link } from "react-router-dom";
 import "./topbar.css";
 import modelInstance from "../data/Model";
 
